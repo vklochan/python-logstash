9442ae75a3edba2eed061f6224bb1873fddf9f38
Vladimir Klochan <vklochan@kmionline.com>
2014-06-23 10:08:42 -0400

    Updated version info.

--- README.rst
+++ README.rst
@@ -6,6 +6,8 @@ http://logstash.net/
 
 Changelog
 =========
+0.4.1
+  - Added TCP handler.
 0.3.1
   - Added support for Python 3
 0.2.2
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
     name='python-logstash',
     packages=['logstash'],
-    version='0.3.1',
+    version='0.4.1',
     description='Python logging handler for Logstash.',
     long_description=open('README.rst').read(),
     author='Volodymyr Klochan',

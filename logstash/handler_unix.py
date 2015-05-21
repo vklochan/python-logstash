938c70e483ec614afe9901cadd439ce629b43aff
michael <michael@adience.com>
2015-05-21 16:19:16 +0300

    Simplified

--- logstash/handler_unix.py
+++ logstash/handler_unix.py
@@ -12,7 +12,7 @@ class UnixLogstashHandler(Handler, object):
     :param socket_name: The name of the unix socket to use.
     """
 
-    def __init__(self, socket_name, formatter_class=formatter.MiniLogstashFormatter, **kwargs):
+    def __init__(self, socket_name, formatter_class=formatter.MiniLogstashFormatter, cpickle=False, **kwargs):
         """
         Initialize a handler.
         """
@@ -22,12 +22,7 @@ class UnixLogstashHandler(Handler, object):
 
         self.sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
         self.sock.settimeout(2)
-        open('/tmp/UnixLogstashHandler.log', 'ab').write('Connecting\n')
         self.sock.connect(socket_name)
-        open('/tmp/UnixLogstashHandler.log', 'ab').write('Connected\n')
-        #self.socket_name = socket_name
-
-        #self.output_file = open('/tmp/UnixLogstashHandler.log', 'wb')
 
     def emit(self, record):
         """
@@ -43,35 +38,3 @@ class UnixLogstashHandler(Handler, object):
             self.sock.close()
         except:
             pass
-
-        """
-        backoff_time = 0.001
-        sent = False
-
-        while not sent:
-            try:
-                self.sock.sendall(formatted_record)
-                sent = True
-            except socket.error as e:
-                self.output_file.write('Got socket.error number %d, Sleeping %.3f\n' % (e.errno, backoff_time))
-                self.output_file.flush()
-            except IOError:
-                self.output_file.write('Got IOError, Sleeping %.3f\n' % backoff_time)
-                self.output_file.flush()
-
-            if not sent:
-                self.sock.close()
-                time.sleep(backoff_time)
-                backoff_time *= (1.5 + 1 * random.random())
-
-                try:
-                    self.sock.connect(self.socket_name)
-                    self.output_file.write("Connecting again to %s\n" % self.socket_name)
-                    self.output_file.flush()
-                except IOError as (no, strerror):
-                    self.output_file.write("I/O error({0}): {1}\n".format(no, strerror))
-                    self.output_file.flush()
-
-                    self.output_file.write('self.sock.connect failed\n')
-                    self.output_file.flush()
-        """

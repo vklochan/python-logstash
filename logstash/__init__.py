f187c263aa9f4df0c2a40a05e2e9f5ab4f2b0c98
Yigal <yigal@adience.com>
2015-08-16 14:42:20 +0300

    Added ArgsLogger to __init__

--- logstash/__init__.py
+++ logstash/__init__.py
@@ -5,6 +5,7 @@ from logstash.handler_tcp import TCPLogstashHandler
 from logstash.handler_udp import UDPLogstashHandler, LogstashHandler
 from logstash.handler_unix import UnixLogstashHandler
 from logstash.handler_zmq import ZmqLogstashHandler
+from logstash.structured_logger import ArgsLogger
 try:
     from logstash.handler_amqp import AMQPLogstashHandler
 except:

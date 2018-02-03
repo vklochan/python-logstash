acee185f3f7571a88a3e7273cf7ca391aad0f83a
José Luis <alagunasalahaddin@live.com>
2018-02-03 04:27:23 +0300

    Update setup.py
    It's missing for the `amqp` handler.

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from distutils.core import setup
 setup(
     name='python-logstash',
     packages=['logstash'],
+    install_requires=['pika'],
     version='0.4.7',
     description='Python logging handler for Logstash.',
     long_description=open('README.rst').read(),
